f3fb970185e704255650a692d3a5384c9546a9c6
Jonasz Karwacki <jonaszakr@gmail.com>
2019-08-29 16:43:26 -0700

    Fix patched input issue

--- tests.py
+++ tests.py
@@ -1,24 +1,48 @@
 from driver import *
 import unittest
-from unittest.mock import patch
+from unittest import mock
 
 class TestSuite(unittest.TestCase):
-    def setUp(self):
-        self.checklist = list()
 
     def test_valid_key_sanitization(self):
         self.assertTrue(sanitize('k'))
 
     def test_invalid_key_sanitization(self):
-        self.assertEquals(sanitize('!'), None)
+        self.assertEqual(sanitize('!'), None)
 
-    @patch('add', return_value='anything')
-    def test_add_to_list(self):
+    @mock.patch('driver.add', mock.MagicMock(return_value='anything'))
+    def test_valid_add(self):
         test = list()
-        add(test)
-        self.assertEquals(len(test), 1)
-        self.assertEquals(test[0], 'anything')
+        # add(test)
+        self.assertEqual(len(test), 1)
+        self.assertEqual(test[0], 'anything')
 
+    def test_invalid_add(self):
+        pass
+
+    def test_valid_update(self):
+        pass
+
+    def test_invalid_update(self):
+        pass
+
+    def test_valid_remove(self):
+        pass
+
+    def test_invalid_remove(self):
+        pass
+
+    def test_sanitize(self):
+        pass
+
+    def test_valid_numerical_input(self):
+        checklist = [1]
+        self.assertTrue(numerical_input_is_valid('0', checklist))
+
+    def test_invalid_numerical_input(self):
+        checklist = [1]
+        self.assertFalse(numerical_input_is_valid('$', checklist))
+        self.assertFalse(numerical_input_is_valid('d', checklist))
 
 if __name__ == '__main__':
     unittest.main()
