5228e6ee2e55f0c3144c0cc84cfeea284cb0bf0e
Jonasz Karwacki <jonaszakr@gmail.com>
2019-08-30 10:33:36 -0700

    Restructure code to bubble up exceptions

--- driver.py
+++ driver.py
@@ -2,7 +2,11 @@
 def display_instructions():
     print('Press A to add to list, R to remove, U to update, and Q to quit')
 
-def numerical_input_is_valid(idx, checklist):
+def index(idx, checklist):
+    '''
+    Takes a string as input. Raises exceptions if that string doesn't represent
+    a valid index.
+    '''
     try:
         int(idx)
     except:
@@ -11,7 +15,6 @@ def numerical_input_is_valid(idx, checklist):
         if int(idx) in range(len(checklist)):
             return True
         raise IndexError(f'That index isn\'t valid for your checklist, which has a length of {len(checklist)}')
-    return False
 
 def sanitize(input):
     if ord(input) in range(ord('A'), ord('z')+1):
@@ -25,19 +28,20 @@ def add(checklist):
         raise ValueError('That\'s not a valid input.')
 
 def remove(checklist):
-    idx = input('Index of item to remove: ')
-    if numerical_input_is_valid(idx, checklist):
-        del checklist[int(idx)]
-    else:
-        raise ValueError('That\'s not a valid input.')
+    try:
+        idx = index(input('Index of item to remove: '))
+        del checklist[idx]
+    except (ValueError, IndexError) as e:
+        raise e
 
 def update(checklist):
-    idx = input('Index of item to update: ')
-    if numerical_input_is_valid(idx, checklist):
+    try:
+        idx = index(input('Index of item to update: '))
+    except (ValueError, IndexError) as e:
+        raise e
+    else:
         val = input('Value you\'d like to update this item to')
         checklist[idx] = val
-    else:
-        raise ValueError('That\'s not a valid input.')
 
 def pretty_format(checklist):
     for i, elm in enumerate(checklist):
