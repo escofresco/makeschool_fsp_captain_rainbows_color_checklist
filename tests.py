d054a1e57188e1a1ae7d6bbcaf1e3ad243373d67
Jonasz Karwacki <jonaszakr@gmail.com>
2019-08-30 12:17:07 -0700

    Add valid and invalid test cases for remove func

--- tests.py
+++ tests.py
@@ -2,7 +2,16 @@ from driver import *
 import unittest
 from unittest import mock
 
+# def decorator(arg):
+#     def real_decorator(func):
+#         def wrapper(*args, **kwargs):
+#             mock.patch('builtins.input', mock.MagicMock(return_value=*args))()
+#         return wrapper
+#     return real_decorator
+
 class TestSuite(unittest.TestCase):
+
+    VALID_CHECKLIST_ITEM = 'Clean the paper towels'
 
     def test_valid_key_sanitization(self):
         self.assertTrue(sanitize('k'))
@@ -10,39 +19,53 @@ class TestSuite(unittest.TestCase):
     def test_invalid_key_sanitization(self):
         self.assertEqual(sanitize('!'), None)
 
-    @mock.patch('driver.add', mock.MagicMock(return_value='anything'))
+    @mock.patch('builtins.input',
+                mock.MagicMock(return_value=VALID_CHECKLIST_ITEM))
     def test_valid_add(self):
         test = list()
-        # add(test)
+        add(test)
         self.assertEqual(len(test), 1)
-        self.assertEqual(test[0], 'anything')
+        self.assertEqual(test[0], self.VALID_CHECKLIST_ITEM)
 
+    @mock.patch('builtins.input', mock.MagicMock(return_value=''))
     def test_invalid_add(self):
-        pass
+        test = list()
+        with self.assertRaises(ValueError):
+            add(test)
 
-    def test_valid_update(self):
-        pass
+    # @mock.patch('builtins.input', mock.MagicMock(return_value='0'))
+    # def test_valid_update(self):
+    #     test = list(['Rainbows are a hoax'])
+    #
+    #     update(test)
+    #     self.assertEqual(test[0], self.VALID_CHECKLIST_ITEM)
 
     def test_invalid_update(self):
         pass
 
+    @mock.patch('builtins.input', mock.MagicMock(return_value='0'))
     def test_valid_remove(self):
-        pass
+        test = list([self.VALID_CHECKLIST_ITEM])
+        remove(test)
+        self.assertEqual(len(test), 0)
 
+    @mock.patch('builtins.input', mock.MagicMock(return_value='0'))
     def test_invalid_remove(self):
-        pass
+        test = list()
+        with self.assertRaises(IndexError):
+            remove(test)
 
     def test_sanitize(self):
         pass
 
     def test_valid_numerical_input(self):
-        checklist = [1]
-        self.assertTrue(numerical_input_is_valid('0', checklist))
+        checklist = [self.VALID_CHECKLIST_ITEM]
+        self.assertEqual(index('0', checklist), 0)
 
-    def test_invalid_numerical_input(self):
-        checklist = [1]
-        self.assertFalse(numerical_input_is_valid('$', checklist))
-        self.assertFalse(numerical_input_is_valid('d', checklist))
+    # def test_invalid_numerical_input(self):
+    #     checklist = [1]
+    #     self.assertFalse(numerical_input_is_valid('$', checklist))
+    #     self.assertFalse(numerical_input_is_valid('d', checklist))
 
 if __name__ == '__main__':
     unittest.main()
