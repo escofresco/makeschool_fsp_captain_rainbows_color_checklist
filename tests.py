f76b38ed78a12ea8f98e4b530c0b1548fcb1332f
Jonasz Karwacki <jonaszakr@gmail.com>
2019-08-31 09:43:14 -0700

    Restructure code to handle global checklist for testing

--- tests.py
+++ tests.py
@@ -3,34 +3,40 @@ from driver import *
 import unittest
 from unittest import mock
 
+
+
 class TestSuite(unittest.TestCase):
 
     STRIKETHROUGH = '\u0336'
     VALID_CHECKLIST_ITEM = 'Drive the wheels'
-    VALID_INCOMPLETE_CHECKLIST_ITEM0 = 'Clean the paper towels'
-    VALID_COMPLETE_CHECKLIST_ITEM1 = (STRIKETHROUGH.join('Walk the leash')+
+    VALID_INCOMPLETE_CHECKLIST_ITEM = 'Clean the paper towels'
+    VALID_COMPLETE_CHECKLIST_ITEM = (STRIKETHROUGH.join('Walk the leash')+
                                       STRIKETHROUGH)
     STATIC_CHECKLIST = [{
-        'content': VALID_INCOMPLETE_CHECKLIST_ITEM1,
+        'content': VALID_INCOMPLETE_CHECKLIST_ITEM,
         'is_complete': False,
     }, {
-        'content': VALID_COMPLETE_CHECKLIST_ITEM1,
+        'content': VALID_COMPLETE_CHECKLIST_ITEM,
         'is_complete': True,
     }]
-    editable_checklist = deepcopy(DEFAULT_CHECKLIST)
+    editable_checklist = deepcopy(STATIC_CHECKLIST)
+
+    def reset_checklist(self):
+        self.editable_checklist = deepcopy(self.STATIC_CHECKLIST)
 
     @mock.patch('builtins.input',
                 mock.MagicMock(return_value=VALID_CHECKLIST_ITEM))
     def test_valid_add(self):
-        add(editable_checklist)
-        self.assertEqual(len(editable_checklist), 3)
-        self.assertEqual(test[-1]['content'], self.VALID_CHECKLIST_ITEM)
+        self.reset_checklist()
+        add(self.editable_checklist)
+        self.assertEqual(len(self.editable_checklist), 3)
+        self.assertEqual(self.editable_checklist[-1]['content'],
+                         self.VALID_CHECKLIST_ITEM)
 
     @mock.patch('builtins.input', mock.MagicMock(return_value=''))
     def test_invalid_add(self):
-        test = list()
         with self.assertRaises(ValueError):
-            add(test)
+            add(self.editable_checklist)
 
     @mock.patch('builtins.input', mock.MagicMock(return_value='0'))
     def test_valid_update(self):
@@ -42,21 +48,20 @@ class TestSuite(unittest.TestCase):
 
     @mock.patch('builtins.input', mock.MagicMock(return_value='0'))
     def test_valid_remove(self):
-        test = list([self.VALID_CHECKLIST_ITEM])
-        remove(test)
-        self.assertEqual(len(test), 0)
+        self.reset_checklist()
+        remove(self.editable_checklist)
+        self.assertEqual(len(self.editable_checklist), 1)
 
     @mock.patch('builtins.input', mock.MagicMock(return_value='0'))
     def test_invalid_index_remove(self):
-        test = list()
         with self.assertRaises(IndexError):
-            remove(test)
+            remove([])
 
     @mock.patch('builtins.input', mock.MagicMock(return_value='$'))
     def test_invalid_character_remove(self):
-        test = list()
+        self.reset_checklist()
         with self.assertRaises(ValueError):
-            remove(test)
+            remove(self.editable_checklist)
 
     def test_valid_sanitize(self):
         for char in ['a', 'r', 'u']:
@@ -68,20 +73,20 @@ class TestSuite(unittest.TestCase):
                 sanitize(char)
 
     def test_valid_index_input(self):
-        checklist = [self.VALID_CHECKLIST_ITEM]
-        self.assertEqual(index('0', checklist), 0)
+        self.reset_checklist()
+        self.assertEqual(index('0', self.editable_checklist), 0)
 
     def test_invalid_index_input(self):
-        checklist = [self.VALID_CHECKLIST_ITEM]
-        for input in ['-1', '1']:
+        self.reset_checklist()
+        for input in ['-1', '2']: # Program doesn't handle indexing from the end
             with self.assertRaises(IndexError):
-                index(input, checklist)
+                index(input, self.editable_checklist)
 
     def test_invalid_char_for_index_input(self):
-        checklist = [self.VALID_CHECKLIST_ITEM]
+        self.reset_checklist()
         for input in ['`!', ')(())()']:
             with self.assertRaises(ValueError):
-                index(input, checklist)
+                index(input, self.editable_checklist)
 
 if __name__ == '__main__':
     unittest.main()
